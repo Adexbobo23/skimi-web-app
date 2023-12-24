dd033ec716ffc46a7c3ddca8f784094867909619
Adeoluwa <oliyideadeoluwajohn1@gmail.com>
2023-12-24 12:14:01 +0100

    New Update version

--- facebook_prj/settings.py
+++ facebook_prj/settings.py
@@ -38,8 +38,7 @@ CHANNEL_LAYERS = {
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
-    'corsheaders', 
+    'jazzmin', 
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -60,15 +59,7 @@ INSTALLED_APPS = [
 
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    # "https://skimi-6cbfc2b63ece.herokuapp.com",
-    "*",
-]
-
-
 MIDDLEWARE = [
-    'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
