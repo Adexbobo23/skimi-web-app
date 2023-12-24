9501e58e5b598ab892fbcb45f57c34d30834d6cd
Adeoluwa <oliyideadeoluwajohn1@gmail.com>
2023-12-24 20:25:03 +0100

    New Update version

--- facebook_prj/settings.py
+++ facebook_prj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vvc&xghblt&%zr_+kmutf+b(5qs%ze5k=kw=bi)naqyw)o4w#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['skimi.net', 'www.skimi.net']
 
 ASGI_APPLICATION = 'facebook_prj.asgi.application'
 
