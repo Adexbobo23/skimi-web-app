209bf8aacb9796c1197ea7b5b1520d9f27e44849
Adeoluwa <oliyideadeoluwajohn1@gmail.com>
2023-12-25 01:16:22 +0100

    Updated the chat layer

--- facebook_prj/settings.py
+++ facebook_prj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vvc&xghblt&%zr_+kmutf+b(5qs%ze5k=kw=bi)naqyw)o4w#7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
